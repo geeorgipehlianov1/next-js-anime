6af1557d4f62022999c7d3bffc5179f5547b1ec3
Georgi Pehlivanov <george9040@abv.bg>
2024-06-06 17:17:08 +0300

    first commit

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,16 +1,38 @@
-import Image from "next/image";
+'use client';
+import { fetchAnime } from '@/app/action';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
+let page = 2;
+
+export type AnimaCard = JSX.Element;
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimaCard[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(2).then((res) => {
+        setData([...data, ...res]);
+        page++;
+      });
+    }
+  }, [inView, data]);
+
   return (
     <>
-      <section className="flex justify-center items-center w-full">
-        <div>
+      <section className='grid grid-cols-1 gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {data}
+      </section>
+      <section className='flex w-full items-center justify-center'>
+        <div ref={ref}>
           <Image
-            src="./spinner.svg"
-            alt="spinner"
+            src='./spinner.svg'
+            alt='spinner'
             width={56}
             height={56}
-            className="object-contain"
+            className='object-contain'
           />
         </div>
       </section>
